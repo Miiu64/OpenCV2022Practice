fdfead3d55a61a1f45b9de92121c89eabfb8dfa9
Vikram <vikram@nandi.net>
2022-01-29 12:13:59 -0800

    added photos and videos

--- read.py
+++ read.py
@@ -1,7 +1,15 @@
+# = Actual Comment
+## = Line of code commented
+
 import cv2 as cv        #imports cv2 but named as cv
 
-img = cv.imread('')     #Reads an image in a path and returns it as a matrix of pixels
+#Reading images
 
-cv.imshow()             #Shows the image in a new window named after the paramter
+##img = cv.imread('Photos\City_Skyline.jpg')     #Reads an image in a path and returns it as a matrix of pixels
+
+##cv.imshow('City Skyline', img)             #Shows the image in a new window named after the paramter
 
 cv.waitKey(0)           #Waits an infinite amount of time for a key to pressed on a keyboard
+
+#Reading videos
+
