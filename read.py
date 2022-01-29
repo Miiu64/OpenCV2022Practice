10ebe1a53838b736707aca3d998afa8621cda0d1
Vikram <vikram@nandi.net>
2022-01-29 12:17:41 -0800

    organized comments

--- read.py
+++ read.py
@@ -6,10 +6,10 @@ import cv2 as cv
 
 
 #Reads an image in a path and returns it as a matrix of pixels
-##img = cv.imread('Photos\City_Skyline.jpg')
+#img = cv.imread('Photos\City_Skyline.jpg')
 
 #Shows the image in a new window named after the paramter
-##cv.imshow('City Skyline', img)
+#cv.imshow('City Skyline', img)
 
 #Waits an infinite amount of time for a key to pressed on a keyboard
 cv.waitKey(0) 
